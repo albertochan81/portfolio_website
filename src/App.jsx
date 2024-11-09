969dc8ecce12af99cb5bc212d5ad43ab66da68c1
albertochan81 <alberto.chanliu81@myhunter.cuny.edu>
2024-11-09 13:07:08 -0500

    Navigation menu changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,13 @@ import Hamburger from 'hamburger-react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
+import Nav from "./components/Nav.jsx";
 import Layout from "./components/Layout.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Nav/>
       <Routes>
         <Route path="/" element={<Home />}>
          
---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Navbar } from 'react-bootstrap';
+import Hamburger from 'hamburger-react';
+
+
+//const Nav = () => {
+  function Nav(){
+  const [isOpen, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen(!isOpen);
+  };
+  
+return (
+
+      <Navbar>
+      
+        <Hamburger toggled={isOpen} toggle={handleToggle} />
+        {isOpen &&(
+          <div className='navContainer'>
+            <ul className='nav' >
+              <li>Home</li>
+              <li>About me</li>
+              <li>Resume</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        )}
+      </Navbar>
+
+    )
+    }
+  
+  export default Nav
+  
