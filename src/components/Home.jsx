85834c6a790abefa25a49e65154213cfa1439955
albertochan81 <alberto.chanliu81@myhunter.cuny.edu>
2025-08-05 20:38:01 -0400

    Home and Resume updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,21 +19,23 @@ function Home () {
           <div>
             <br/> <br/>          
             <Row>
-              <img className='profilePic' src= 'https://raw.githubusercontent.com/albertochan81/portfolio_website/refs/heads/main/public/Images/profile_pic.jpg'/>
+              <img className='profilePic' src= '../images/profile-pic.jpg'/>
             </Row>
           </div>
             <br/> 
             <h1>Alberto Chan Liu</h1>
-            <h2>Computer Science Graduate</h2>
+            <h2>Airport Systems Support Technician</h2>
             <br/>
 
           <div className='about-section'>
             <h3>About Me</h3>
-                <p>Hi, I’m Alberto Chan Liu, a recent Computer Science graduate with a passion for technology, problem-solving, and continuous learning. I was born and raised in Venezuela and later came to the United States to pursue my education and build a better future.</p>
+                <p>Hi, I’m Alberto Chan Liu, a Computer Science graduate with a passion for technology, problem-solving, and continuous learning. I was born and raised in Venezuela and later came to the United States to pursue my education and build a better future.</p>
 
                 <p>Before transitioning fully into tech, I worked as an inventory manager at a beauty supply warehouse, where I developed strong skills in organization, efficiency, and operations management. My diverse background has shaped my ability to approach challenges with creativity and practicality.</p>
 
-                <p>Currently, I am actively involved in testing projects to strengthen my hands-on experience, and I am also pursuing professional certifications, such as CompTIA, to further build my technical skills and industry knowledge.</p>
+                <p>I currently work as an Airport Systems Support Technician, where I provide front-line IT support for various airport technologies. This role has strengthened my technical troubleshooting skills and given me real-world experience with critical infrastructure systems.</p>
+
+                <p>I’ve earned both the CompTIA A+ and AWS Cloud Practitioner certifications, and I continue to build my knowledge through hands-on projects and ongoing learning in areas like networking and cybersecurity.</p>
 
                 <p>Whether it’s building software, solving problems, or exploring innovative ideas, I’m always looking for ways to grow and make an impact.</p>
             </div>
